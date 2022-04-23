4bd5ca38dfa842271518a1de1aabbb95a0867e1e
Yohan Prakoso <prakoso.yohan@gmail.com>
2022-04-24 00:13:52 +0800

    modified something

--- simpleKeyStroke.py
+++ simpleKeyStroke.py
@@ -3,18 +3,17 @@ import sys, select, termios, tty
 def getKey(key_timeout):
     tty.setraw(sys.stdin.fileno())
     rlist, _, _ = select.select([sys.stdin], [], [], key_timeout)
-    # print(rlist)
     if rlist:
         key = sys.stdin.read(1)
     else:
         key = ''
-    # termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
 settings = termios.tcgetattr(sys.stdin)
 while 1:
     key = getKey(None)
-    print(key+"\r")
+    print(key)
 
     if (key == '\x03'):
         break
